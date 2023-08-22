2b8bbddfa99af1dfb3dc9d5b431dfba25d298a80
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2023-08-22 14:10:21 -0700

    formatting strings
    formatting strings

--- main.go
+++ main.go
@@ -47,6 +47,33 @@ func main() {
 	scoreThree := 1.2 // inferred as float64
 
 	fmt.Println(scoreOne, scoreTwo, scoreThree)
+
+	// Print
+	fmt.Print("Hello, ")
+	fmt.Print("world!") // print on same line
+
+	fmt.Print("Hello, \n")
+
+	// Print line
+	fmt.Println("world! \n") // print on new line
+
+	fmt.Println("Hello, world!") // prints on new line automatically
+	fmt.Println("New, line!")
+
+	fmt.Println("my age is", ageOne, "and my name is", nameOne) // prints variables
+
+	// Printf (formatted strings) - %_ = format specifier
+	fmt.Printf("my age is %v and my name is %v \n", ageOne, nameOne) // %v = value, values in order
+
+	fmt.Printf("my age is %v and my name is %q \n", ageOne, nameOne)
+
+	fmt.Printf("age is of type %T \n", ageOne) // %T = type
+
+	fmt.Printf("you scored %0.1f points! \n", scoreOne) // %f = float
+
+	// Sprintf (save formatted strings)
+	var str = fmt.Sprintf("my age is %v and my name is %v \n", ageOne, nameOne)
+	fmt.Println("the saved string is:", str)
 }
 
 // go run main.go
