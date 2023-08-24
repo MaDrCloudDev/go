68908c2bbf0e08d78b9c9a58c82ecc08abeb1eef
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2023-08-24 11:42:17 -0700

    arrays and slices
    arrays and slices

--- main.go
+++ main.go
@@ -74,6 +74,36 @@ func main() {
 	// Sprintf (save formatted strings)
 	var str = fmt.Sprintf("my age is %v and my name is %v \n", ageOne, nameOne)
 	fmt.Println("the saved string is:", str)
+
+	// Arrays - fixed length
+	var ages [3]int = [3]int{20, 25, 30}
+
+	// shorthand array
+	var agesShort = [3]int{21, 26, 31}
+
+	names := [4]string{"Matt", "Mike", "Nicky", "Bob"}
+	names[1] = "Michael" // change value
+
+	fmt.Println(ages, len(ages))
+	fmt.Println(agesShort, len(agesShort))
+	fmt.Println(names, len(names))
+
+	// Slices - like arrays but can be resized
+	var scores = []int{100, 50, 60} // no length in brackets
+	scores[2] = 25                  // change value
+	scores = append(scores, 85)     // add value
+
+	fmt.Println(scores, len(scores))
+
+	// slice ranges
+	rangeOne := names[1:3]  // 1st index to 3rd index (not including 3rd)
+	rangeTwo := names[2:]   // 2nd index to end
+	rangeThree := names[:3] // start to 3rd index (not including 3rd)
+
+	fmt.Println(rangeOne, rangeTwo, rangeThree)
+
+	rangeOne = append(rangeOne, "Matt") // add value to rangeOne
+	fmt.Println(rangeOne)
 }
 
 // go run main.go
