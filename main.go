15d5f640cf7ce2ab52fc25b631c5702d5981810d
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2023-08-25 15:17:02 -0700

    add loops
    add loops

--- main.go
+++ main.go
@@ -134,6 +134,38 @@ func main() {
 
 	fmt.Println(sort.SearchStrings(namesSort, "Matt")) // returns index of value
 
+	// loops
+	// "while" loop equivalent (but uses "for" in go)
+	x := 0
+	for x < 5 {
+		fmt.Println("value of x is:", x)
+		x++
+	}
+
+	// "for" loop
+	for i := 0; i < 5; i++ {
+		fmt.Println("value of i is:", i)
+	} // does the same thing using for instead of while
+
+	// "for" loop over a slice
+	namesLoop := []string{"Matt", "Mike", "Nicky", "Bob"}
+
+	for i := 0; i < len(namesLoop); i++ {
+		fmt.Println(namesLoop[i])
+	}
+
+	// for-in loop using range
+	for index, value := range namesLoop {
+		fmt.Printf("the value at index %v is %v \n", index, value)
+	}
+
+	// what if we just want to use the value, not the index? use underscore for unwanted values.
+	for _, value := range namesLoop {
+		// value = "new string" // doesn't change original array, just this copy of the value(s)
+		fmt.Printf("the value is %v \n", value)
+	}
+
+	fmt.Println(namesLoop)
 }
 
 // go run main.go
